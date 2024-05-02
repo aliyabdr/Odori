d0033aedc9abbdd8112a3a15435fc165d022148e
herplasticheart <99028927+herplasticheart@users.noreply.github.com>
2024-05-02 15:18:20 +0200

    hallo_welt_überschrift

---
+++ PHP/anzeige.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Odori</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        Hallo Welt
+    </header>
+</body>
+</html>
