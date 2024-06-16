a209222bb59d5d2ea3820d4de0be435a7724ca0f
iniii9 <ingrid09vitra@yahoo.com>
2024-06-16 19:56:25 +0200

    db.php verbessert

--- PHP/db.php
+++ PHP/db.php
@@ -1,5 +1,6 @@
+<?php
 $host = 'localhost'; // Datenbank-Host, normalerweise 'localhost'
-$db   = 'kleinanzeigenplattform'; // Name der Datenbank
+$db = 'kleinanzeigenplattform'; // Name der Datenbank
 $user = 'root'; // Datenbank-Benutzername, standardmäßig 'root' für XAMPP
 $pass = ''; // Datenbank-Passwort, standardmäßig leer für XAMPP
 $charset = 'utf8mb4'; // Zeichensatz
@@ -9,9 +10,9 @@ $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
 // PDO Optionen
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::ATTR_EMULATE_PREPARES => false,
 ];
 
 try {
@@ -21,3 +22,4 @@ try {
     // Fehlerbehandlung
     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
+?>
