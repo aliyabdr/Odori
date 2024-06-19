2ed9e0606ce6b96ac097fab3fc45f7dfeb28b654
iniii9 <ingrid09vitra@yahoo.com>
2024-06-19 17:52:47 +0200

    erstellte ads mit user location verbinden

--- PHP/create_ad.php
+++ PHP/create_ad.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include '../db_connect.php'; // Stelle sicher, dass dieser Pfad korrekt ist
+include '../db_connect.php'; // Verbindung zur Datenbank herstellen
 
 // Überprüfen, ob der Benutzer eingeloggt ist
 if (!isset($_SESSION['user_id'])) {
@@ -17,25 +17,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $brand = $_POST['brand'];
     $condition = $_POST['condition'];
     $user_id = $_SESSION['user_id'];
+
+    // Benutzerinformationen aus der Datenbank abrufen
+    $sql_user = "SELECT location FROM users WHERE id = ?";
+    $stmt_user = $conn->prepare($sql_user);
+    $stmt_user->bind_param("i", $user_id);
+    $stmt_user->execute();
+    $result_user = $stmt_user->get_result();
+    $user = $result_user->fetch_assoc();
+    $stmt_user->close();
+
+    $location = $user['location'];
     
     // Bild-Upload bearbeiten
-    $image_paths = [];
     $upload_directory = '../uploads/';
-    
-    foreach ($_FILES['images']['tmp_name'] as $key => $tmp_name) {
-        $file_name = basename($_FILES['images']['name'][$key]);
-        $target_file = $upload_directory . $file_name;
-        if (move_uploaded_file($tmp_name, $target_file)) {
-            $image_paths[] = $target_file;
-        }
+    $file_name = basename($_FILES['images']['name'][0]);
+    $target_file = $upload_directory . $file_name;
+    if (move_uploaded_file($_FILES['images']['tmp_name'][0], $target_file)) {
+        $image_url = $target_file;
+    } else {
+        $image_url = ''; // Setze einen leeren String, wenn der Bild-Upload fehlschlägt
     }
-    $image_paths_string = implode(',', $image_paths);
 
     // Die Anzeige in die Datenbank einfügen
-    $sql = "INSERT INTO ads (title, description, category, price, color, brand, `condition`, image_url, user_id) 
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO ads (title, description, category, price, color, brand, `condition`, image_url, user_id, location) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sssdsdssi", $title, $description, $category, $price, $color, $brand, $condition, $image_paths_string, $user_id);
+    $stmt->bind_param("sssdsdssis", $title, $description, $category, $price, $color, $brand, $condition, $image_url, $user_id, $location);
     
     if ($stmt->execute()) {
         header('Location: eigenes_profil.php');
@@ -49,6 +57,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 $conn->close();
 ?>
+
 
 
 
